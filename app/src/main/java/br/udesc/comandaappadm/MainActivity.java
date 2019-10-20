a65aed43bd0443a2424e9eb1e148300fdf94e36e
Fabio-Frare <nabata99@hotmail.com>
2019-10-19 22:20:36 -0300

    criado a view menu inicial.

--- app/src/main/java/br/udesc/comandaappadm/MainActivity.java
+++ app/src/main/java/br/udesc/comandaappadm/MainActivity.java
@@ -3,12 +3,28 @@ package br.udesc.comandaappadm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnCategoria;
+    private Button btnConfigProduto;
+    private Button btnProduto;
+    private Button btnListaProdutos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initComponentes();
+
+    }
+
+    private void initComponentes() {
+        btnCategoria = findViewById(R.id.btnCategoria);
+        btnConfigProduto = findViewById(R.id.btnConfigProduto);
+        btnProduto = findViewById(R.id.btnProduto);
+        btnListaProdutos = findViewById(R.id.btnLista);
     }
 }
